08fb20166c1f84e7d803cbb76e0d84c70c0cec63
YASWANTH <yaswanthvanacharla@gmail.com>
2026-01-24 18:54:39 +0530

    added love calculator

---
+++ lovecalculator.py
@@ -0,0 +1,17 @@
+a=input("enter 1st name")
+b=input("enter 2nd name ")
+com=a+b
+low=com.lower()
+print(low)
+t=low.count('t')
+r=low.count('r')
+u=low.count('u')
+e=low.count('e')
+l=low.count('l')
+o=low.count('o')
+v=low.count('v')
+e=low.count('e')
+true=t+r+u+e
+love=l+o+v+e
+true_love=str(true)+str(love)
+print(true_love)
