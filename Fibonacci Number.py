96a7cf8b8610ff7242bfdf0278ce6fe397282006
YASWANTH <yaswanthvanacharla@gmail.com>
2026-03-23 22:10:08 +0530

    Implement Fibonacci function with call count tracking
    Refactored Fibonacci function to use global count for tracking calls.

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -1,6 +1,13 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n==1 or n==0:
-            return n
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+count=0
+def fib(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    global count
+    count+=1
+    sum=fib(n-1)+fib(n-2)
+    return sum
+n=int(input("Which Value you wanna know the fibbinoci:"))
+print(fib(n))
+print(f"Total Number of Times Function Called:{count}")
